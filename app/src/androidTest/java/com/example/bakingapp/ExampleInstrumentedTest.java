ed475e2978cc88705f2d23d963195de62d61378b
Nereo Ontiveros <nerontiveros@gmail.com>
2020-06-24 22:30:54 +0100

    test: Add Espresso testing

--- app/src/androidTest/java/com/example/bakingapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bakingapp/ExampleInstrumentedTest.java
@@ -2,12 +2,22 @@ package com.example.bakingapp;
 
 import android.content.Context;
 
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+
+import com.example.bakingapp.view.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +27,21 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void launchActivity(){
+
+            onView(ViewMatchers.withId(R.id.rv_recipes)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+            onView(ViewMatchers.withId(R.id.rv_steps)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+    }
+
+
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +49,6 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.bakingapp", appContext.getPackageName());
     }
+
+
 }
